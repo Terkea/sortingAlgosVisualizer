4ddb818cb43a71fe166197ae7d91079bccfe85b2
Terkea <terkeabt@gmail.com>
2020-06-20 22:25:11 +0100

    update the board

--- src/components/Board.js
+++ src/components/Board.js
@@ -8,11 +8,11 @@ const Board = (props) => {
       {(value) => {
         const { size, speed, items, dispatch } = value;
         return (
-          <div style={styles.board}>
+            <div style={styles.board}>
             {items.map((value, i) => {
               // the value of the board width divided by the number of values from which we substract 5 (the margin factor per)
               return (
-                <Line key={i} size={value} width={1000 / items.length - 5} />
+                  <Line key={i} size={value.value} width={(1000 / items.length) - 5} />
               );
             })}
           </div>
@@ -25,8 +25,8 @@ const Board = (props) => {
 const styles = {
   board: {
     backgroundColor: 'rgba(255, 0, 0, 0.2)',
-    width: '1000px',
-    height: '700px',
+    width: '1020px',
+    height: '500px',
     margin: 'auto',
     display: 'flex',
   },
