890a1fda642f2ca6903e2545a589dbc2e24f5b72
Terkea <terkeabt@gmail.com>
2020-06-21 18:49:59 +0100

    bubble sort algo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -71,25 +71,21 @@ const NavBar = (props) => {
         const { size, speed, items, dispatch } = value;
         const sort = (algorithm) => {
 
-          var arr = Object.keys(items).map(function (key) {
-            return [Number(key), items[key]];
-          });
-
-          console.log(arr)
-
-          let swapped;
-          do {
-            swapped = false;
-            for (let i = 0; i < arr.length; i++) {
-              if (arr[i].value > arr[i + 1].value) {
-                let tmp = arr[i].value;
-                arr[i].value = arr[i + 1].value;
-                arr[i + 1].value = tmp;
-                swapped = true;
+          var temp_items = items
+          var length = temp_items.length;
+          for (var i = 0; i < length; i++) { //Number of passes
+            for (var j = 0; j < (length - i - 1); j++) { //Notice that j < (length - i)
+              //Compare the adjacent positions
+              if (temp_items[j].value > temp_items[j + 1].value) {
+                //Swap the numbers
+                var tmp = temp_items[j].value; //Temporary variable to hold the current number
+                temp_items[j].value = temp_items[j + 1].value; //Replace current number with adjacent number
+                temp_items[j + 1].value = tmp; //Replace adjacent number with current number
               }
             }
-          } while (swapped);
-          console.log(arr);
+          }
+
+          console.log(items)
         }
 
         return (
