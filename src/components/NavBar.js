a15ec35ed79c7e836020fdad9383fae6aad70319
Terkea <terkeabt@gmail.com>
2020-06-21 23:15:07 +0100

    code cleaning

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,7 +8,19 @@ const SIZE_MINIMUM = 0;
 const SPEED_MAXIMUM = 2000;
 const SPEED_MINIMUM = 20; //ms
 
-const NavBar = (props) => {
+const NavBar = () => {
+
+  const updateItem = (id, size, color, dispatch) => {
+    dispatch({
+      type: 'UPDATE_ITEM',
+      payload: {
+        id: id,
+        value: size,
+        status: color
+      },
+    });
+  }
+
   const onChangeSize = (e, dispatch) => {
     const size = e.target.value;
     if (size > SIZE_MAXIMUM) {
@@ -72,81 +84,24 @@ const NavBar = (props) => {
         //Notice that j < (length - i)
         // Compare the adjacent positions
         await timer(speed);
-        dispatch({
-          type: 'UPDATE_ITEM',
-          payload: {
-            id: temp_items[j].id,
-            value: temp_items[j].value,
-            status: 'evaluating',
-          },
-        });
-        dispatch({
-          type: 'UPDATE_ITEM',
-          payload: {
-            id: temp_items[j + 1].id,
-            value: temp_items[j + 1].value,
-            status: 'evaluating',
-          },
-        });
+        updateItem(temp_items[j].id, temp_items[j].value, 'evaluating', dispatch)
+        updateItem(temp_items[j + 1].id, temp_items[j + 1].value, 'evaluating', dispatch)
         if (temp_items[j].value > temp_items[j + 1].value) {
           await timer(speed / 2);
           //Swap the numbers
           var tmp = temp_items[j].value; //Temporary variable to hold the current number
           temp_items[j].value = temp_items[j + 1].value; //Replace current number with adjacent number
           temp_items[j + 1].value = tmp; //Replace adjacent number with current number
-          dispatch({
-            type: 'UPDATE_ITEM',
-            payload: {
-              id: temp_items[j].id,
-              value: temp_items[j].value,
-              status: 'evaluating',
-            },
-          });
-          dispatch({
-            type: 'UPDATE_ITEM',
-            payload: {
-              id: temp_items[j + 1].id,
-              value: temp_items[j + 1].value,
-              status: 'error',
-            },
-          });
+          updateItem(temp_items[j].id, temp_items[j].value, 'evaluating', dispatch)
+          updateItem(temp_items[j + 1].id, temp_items[j + 1].value, 'error', dispatch)
 
           await timer(speed / 2);
-          dispatch({
-            type: 'UPDATE_ITEM',
-            payload: {
-              id: temp_items[j].id,
-              value: temp_items[j].value,
-              status: 'default',
-            },
-          });
-          dispatch({
-            type: 'UPDATE_ITEM',
-            payload: {
-              id: temp_items[j + 1].id,
-              value: temp_items[j + 1].value,
-              status: 'default',
-            },
-          });
+          updateItem(temp_items[j].id, temp_items[j].value, 'default', dispatch)
+          updateItem(temp_items[j + 1].id, temp_items[j + 1].value, 'default', dispatch)
         } else {
           await timer(speed);
-
-          dispatch({
-            type: 'UPDATE_ITEM',
-            payload: {
-              id: temp_items[j].id,
-              value: temp_items[j].value,
-              status: 'default',
-            },
-          });
-          dispatch({
-            type: 'UPDATE_ITEM',
-            payload: {
-              id: temp_items[j + 1].id,
-              value: temp_items[j + 1].value,
-              status: 'default',
-            },
-          });
+          updateItem(temp_items[j].id, temp_items[j].value, 'default', dispatch)
+          updateItem(temp_items[j].id, temp_items[j].value, 'default', dispatch)
         }
       }
     }
@@ -217,7 +172,6 @@ const NavBar = (props) => {
                 break;
               default:
                 algorithm = null;
-                break;
             }
             if (algorithm != null) {
               algorithm(items, speed, dispatch);
