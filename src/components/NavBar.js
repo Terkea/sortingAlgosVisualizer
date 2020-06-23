fe1e4e17f52f6b9745ee66e3c66c0ba2415bee8a
Terkea <terkeabt@gmail.com>
2020-06-23 19:26:16 +0100

    selection sort

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -134,11 +134,11 @@ const NavBar = () => {
         payload: temp_items,
       });
       while (j >= 0 && temp_items[j].value > temp) {
-        await timer(speed/2);
+        await timer(speed / 2);
         updateItem(temp_items[j].id, temp_items[j].value, 'evaluating', dispatch)
         updateItem(temp_items[j + 1].id, temp_items[j + 1].value, 'evaluating', dispatch)
 
-        await timer(speed/2);
+        await timer(speed / 2);
         updateItem(temp_items[j].id, temp_items[j].value, 'evaluating', dispatch)
         updateItem(temp_items[j + 1].id, temp_items[j + 1].value, 'error', dispatch)
 
@@ -166,7 +166,51 @@ const NavBar = () => {
   }
 
   async function selectionSort(items, speed, dispatch) {
-    console.log('Preparing to selectionSort');
+    var temp_items = items;
+    var length = temp_items.length;
+    dispatch({
+      type: 'UPDATE_SORTING',
+      payload: true,
+    });
+
+    for (let i = 0; i < length; i++) {
+      let min = i;
+      await timer(speed);
+      updateItem(temp_items[min].id, temp_items[min].value, 'error', dispatch)
+
+      for (let j = i + 1; j < length; j++) {
+        if (temp_items[min].value > temp_items[j].value) {
+          await timer(speed / 2);
+          updateItem(temp_items[j].id, temp_items[j].value, 'evaluating', dispatch)
+
+          await timer(speed / 2);
+          updateItem(temp_items[j].id, temp_items[j].value, 'default', dispatch)
+
+          min = j;
+        }
+      }
+      if (min !== i) {
+        let tmp = temp_items[i].value;
+        temp_items[i].value = temp_items[min].value;
+        temp_items[min].value = tmp;
+        updateItem(temp_items[i].id, temp_items[i].value, 'default', dispatch)
+        dispatch({
+          type: 'UPDATE_LIST',
+          payload: temp_items,
+        });
+      }
+    }
+    dispatch({
+      type: 'UPDATE_LIST',
+      payload: temp_items,
+    });
+    //prevent from sorting multiple times
+    dispatch({
+      type: 'UPDATE_SORTING',
+      payload: false,
+    });
+
+    console.log('selection shit')
   }
 
   async function mergeSort(items, speed, dispatch) {
